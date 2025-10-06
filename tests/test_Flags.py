f65316acca3420eb3b15504a1c823ea0501eac3f
Elliott Bache <elliottbache@gmail.com>
2025-10-06 18:56:01 +0200

    Created separate test for simple inputs.

--- tests/test_Flags.py
+++ tests/test_Flags.py
@@ -1,7 +1,10 @@
 from Flags import flags_solution
 
-def test_flags_solution():
+def test_base_case():
 
     assert flags_solution([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]) == 3
+
+def test_simple_input():
+
     assert flags_solution([1, 3, 2]) == 1
     assert flags_solution([3, 2, 1]) == 0
