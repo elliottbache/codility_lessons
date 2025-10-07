22996f71c544c0d167548798d72626ab2c5b1519
Elliott Bache <elliottbache@gmail.com>
2025-10-07 11:35:36 +0200

    Implemented parametrization in Flag tests.

--- tests/test_Flags.py
+++ tests/test_Flags.py
@@ -2,23 +2,10 @@ import pytest
 
 from Flags import flags_solution
 
-@pytest.fixture
-def base_case():
-    return [1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]
-
-@pytest.fixture
-def simple_input_one_peak():
-    return [1, 3, 2]
-
-@pytest.fixture
-def simple_input_no_peaks():
-    return [3, 2, 1]
-
-def test_base_case(base_case):
-
-    assert flags_solution(base_case) == 3
-
-def test_simple_input(simple_input_one_peak,simple_input_no_peaks):
-
-    assert flags_solution(simple_input_one_peak) == 1
-    assert flags_solution(simple_input_no_peaks) == 0
+@pytest.mark.parametrize("A, expected", [
+    ([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2], 3),
+    ([1, 3, 2], 1),
+    ([3, 2, 1], 0)
+])
+def test_flags_solution(A, expected):
+    assert flags_solution(A) == expected
