2e101082a45cc3d0eeb55883992f315c4d3cf0d6
Elliott Bache <elliottbache@gmail.com>
2025-10-11 22:07:50 +0200

    Ran flake8 on count_semiprimes.py.

--- count_semiprimes.py
+++ count_semiprimes.py
@@ -1,11 +1,17 @@
 """
-A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
+A prime is a positive integer X that has exactly two distinct divisors:
+1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
 
-A semiprime is a natural number that is the product of two (not necessarily distinct) prime numbers. The first few semiprimes are 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
+A semiprime is a natural number that is the product of two (not
+necessarily distinct) prime numbers. The first few semiprimes are 4, 6,
+9, 10, 14, 15, 21, 22, 25, 26.
 
-You are given two non-empty arrays P and Q, each consisting of M integers. These arrays represent queries about the number of semiprimes within specified ranges.
+You are given two non-empty arrays P and Q, each consisting of M
+integers. These arrays represent queries about the number of semiprimes
+within specified ranges.
 
-Query K requires you to find the number of semiprimes within the range (P[K], Q[K]), where 1 ≤ P[K] ≤ Q[K] ≤ N.
+Query K requires you to find the number of semiprimes within the range
+(P[K], Q[K]), where 1 ≤ P[K] ≤ Q[K] ≤ N.
 
 For example, consider an integer N = 26 and arrays P, Q such that:
 
@@ -24,7 +30,9 @@ Write a function:
 def solution(N, P, Q)
 content_copy
 
-that, given an integer N and two non-empty arrays P and Q consisting of M integers, returns an array consisting of M elements specifying the consecutive answers to all the queries.
+that, given an integer N and two non-empty arrays P and Q consisting of
+M integers, returns an array consisting of M elements specifying the
+consecutive answers to all the queries.
 
 For example, given an integer N = 26 and arrays P, Q such that:
 
@@ -80,7 +88,7 @@ def count_semiprimes_solution(N: int, P: List[int], Q: List[int]) -> List[int]:
     # create list with number of semiprimes at current index
     semiprime_count = [0] * (N+1)
     j = 0
-    for i in range(1,len(semiprime_count)):
+    for i in range(1, len(semiprime_count)):
         if j < len(semiprimes) and i == semiprimes[j]:
             semiprime_count[i] = semiprime_count[i-1] + 1
             j += 1
@@ -91,10 +99,11 @@ def count_semiprimes_solution(N: int, P: List[int], Q: List[int]) -> List[int]:
     results = list()
     for i in range(len(P)):
 
-        idx = max(P[i] - 1,0)
+        idx = max(P[i] - 1, 0)
         results.append(semiprime_count[Q[i]]-semiprime_count[idx])
 
     return results
+
 
 def sieve(n):
     """Create list of prime numbers from 1 to n.
@@ -129,9 +138,10 @@ def sieve(n):
 
     return primes
 
+
 if __name__ == '__main__':
     N = 26
     P = [1, 4, 16]
     Q = [26, 10, 20]
-    print(count_semiprimes_solution(N,P,Q))
+    print(count_semiprimes_solution(N, P, Q))
     print(count_semiprimes_solution(1, [1], [1]))
